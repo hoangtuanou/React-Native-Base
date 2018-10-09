2919f4fad8becea3bb2077304f0196c58f2c94ed
Hoang Tuan <tuan.tha@sutrixsolutions.com>
2018-10-09 08:24:53 +0700

    - update config version

---
+++ src/config/update_config.py
@@ -0,0 +1,19 @@
+import os
+import json
+import plistlib
+
+env = os.environ
+
+versionStr = env['VERSION_NO'] + '.' + env['BUILD_NO']
+
+pl = plistlib.readPlist("../../ios/YogaWorks/Info.plist")
+pl['CFBundleShortVersionString'] = versionStr
+pl['CFBundleVersion'] = versionStr
+plistlib.writePlist(pl, "../../ios/YogaWorks/Info.plist")
+
+with open("../../package.json", "r") as jsonFile:
+    data = json.load(jsonFile)
+    data['version'] = versionStr
+
+with open("../../package.json", "w") as jsonFile:
+    json.dump(data, jsonFile, indent=2, separators=(',', ': '))
