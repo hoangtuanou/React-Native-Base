7594aadd74e667aa2c78f80c27147feef9008551
Hoang Tuan <hoanganhtuan.ou@gmail.com>
2018-10-09 08:50:13 +0700

    - fix wrong version config directory.

--- src/config/update_config.py
+++ src/config/update_config.py
@@ -6,14 +6,14 @@ env = os.environ
 
 versionStr = env['VERSION_NO'] + '.' + env['BUILD_NO']
 
-pl = plistlib.readPlist("../../ios/YogaWorks/Info.plist")
+pl = plistlib.readPlist("ios/YogaWorks/Info.plist")
 pl['CFBundleShortVersionString'] = versionStr
 pl['CFBundleVersion'] = versionStr
-plistlib.writePlist(pl, "../../ios/YogaWorks/Info.plist")
+plistlib.writePlist(pl, "ios/YogaWorks/Info.plist")
 
-with open("../../package.json", "r") as jsonFile:
+with open("package.json", "r") as jsonFile:
     data = json.load(jsonFile)
     data['version'] = versionStr
 
-with open("../../package.json", "w") as jsonFile:
+with open("package.json", "w") as jsonFile:
     json.dump(data, jsonFile, indent=2, separators=(',', ': '))
