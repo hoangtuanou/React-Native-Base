d75544c22562184fb522d644da59e51a572596db
Hoang Tuan <hoanganhtuan.ou@gmail.com>
2018-10-09 14:03:14 +0700

    - config version notification.

--- config/deploy/mattermost_notification.sh
+++ config/deploy/mattermost_notification.sh
@@ -1,7 +1,8 @@
+
 payload='{
   "channel": "ci",
   "username": "App Center Bot",
-  "text": "Build YogaWorks $VERSION_NO.$BUILD_NO. @here :tada: $APPCENTER_OUTPUT_DIRECTORY"
+  "text": "Build YogaWorks '$VERSION_NO'.'$BUILD_NO' . @here :tada: '$APPCENTER_OUTPUT_DIRECTORY'"
 }'
 
 curl -i -X POST --data-urlencode "payload=$payload" https://internalchat.sutrix.com/hooks/t4oebs71mbbrtypgcphwypspca
